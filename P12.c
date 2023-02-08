c73ba1fa668329ea7518d13733c425b3088a9bc9
Caleb Janhunen <cjanhune@uoguelph.ca>
2023-02-07 22:41:10 -0500

    Finished mergesort algorithm

--- P12.c
+++ P12.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 
+int mergeSort(int arr[], int low, int high);
+int merge(int arr[], int low, int mid, int high);
+
 int main(int argc, char **argv) {
     FILE *fp;
     char *filename = "data_A2_Q1.txt";
@@ -30,8 +33,73 @@ int main(int argc, char **argv) {
         // printf("%d: %d \n", i, arr[i]);
         i++;
     }
+    // int n = sizeof(arr) / sizeof(arr[0]);
+    // printf("%d \n", n);
+    int count = mergeSort(arr, 0, 50000 - 1);
+    printf("%d \n", count);
+
     return 0;
 }
 
-void mergeSort(int *arr) {
+int mergeSort(int arr[], int low, int high) {
+    int mid;
+    int count = 0;
+
+    if (low < high) {
+        mid = (low + high) / 2;
+        count = mergeSort(arr, low, mid);
+        count += mergeSort(arr, mid + 1, high);
+        count += merge(arr, low, mid, high);
+    }
+
+    return count;
+}
+
+// int mergeSort(int array[], int low, int high) {
+//     int mid, count = 0;
+
+//     if (low < high) {
+//         mid = (low + high) / 2;
+
+//         count = mergeSort(array, low, mid);
+//         count += mergeSort(array, mid + 1, high);
+//         count += merge(array, low, mid, high);
+//     }
+
+//     return count;
+// }
+
+int merge(int arr[], int low, int mid, int high) {
+    // int count = 0;
+    // int temp[high - low + 1];
+    // int i = low, j = mid + 1, k = 0;
+    int i, j, k, count = 0;
+
+    int temp[high - low + 1];
+    i = low;
+    j = mid + 1;
+    k = 0;
+
+    while (i <= mid && j <= high) {
+        if (arr[i] <= arr[j]) {
+            temp[k++] = arr[i++];
+        } else {
+            count += mid - i + 1;
+            temp[k++] = arr[j++];
+        }
+    }
+
+    while (i <= mid) {
+        temp[k++] = arr[i++];
+    }
+
+    while (j <= high) {
+        temp[k++] = arr[j++];
+    }
+
+    for (i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+
+    return count;
 }
