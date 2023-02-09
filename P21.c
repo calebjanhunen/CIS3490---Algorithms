d546d068b90c29c7329defcd9c9d4df9dace7de7
Caleb Janhunen <cjanhune@uoguelph.ca>
2023-02-09 11:08:58 -0500

    commit feb09

--- P21.c
+++ P21.c
@@ -7,12 +7,12 @@ struct Point {
     float y;
 } typedef Point;
 
-Point *convexHullBF(Point *points, int numPoints, Point *hull);
-int cross_product(struct Point a, struct Point b, struct Point c);
+void convexHullBF(Point *points, int numPoints, Point *hull, int *numHullPoints);
 
 int main(int argc, char **argv) {
     FILE *fp;
-    char *filename = "testQ2.txt";
+    // char *filename = "testQ2.txt";
+    char *filename = "data_A2_Q2.txt";
     int fsize;
     Point points[30000];
     char num[20];
@@ -30,12 +30,16 @@ int main(int argc, char **argv) {
     }
 
     Point hull[20];
-    int numPoints = 5;
-    convexHullBF(points, numPoints, hull);
+    int numPoints = 30000, numHullPoints = 0;
+    start = clock();
+    convexHullBF(points, numPoints, hull, &numHullPoints);
+    end = clock();
 
-    for (int i = 0; i < sizeof(hull) / sizeof(Point); i++) {
+    for (int i = 0; i < numHullPoints; i++) {
         printf("x: %f, y: %f \n", hull[i].x, hull[i].y);
     }
+    double duration = ((double)end - start) / CLOCKS_PER_SEC;
+    printf("Execution time = %f \n", duration);
 
     // printf("%d \n", sizeof(hull) / sizeof(Point));
 
@@ -43,50 +47,43 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-Point *convexHullBF(Point *points, int numPoints, Point *hull) {
-    int hullIndex = 0;
+void convexHullBF(Point *points, int numPoints, Point *hull, int *numHullPoints) {
     float a, b, c, d;
     int leftSide, rightSide;
 
     for (int i = 0; i < numPoints; i++) {
-        for (int j = i + 1; j <= numPoints; j++) {
+        printf("%d \n", i);
+
+        for (int j = i + 1; j < numPoints; j++) {
             a = points[j].y - points[i].y;
-            b = points[j].x - points[i].x;
+            b = points[i].x - points[j].x;
             c = (points[i].x * points[j].y) - (points[i].y * points[j].x);
             leftSide = 0;
             rightSide = 0;
-            // printf("x: %f y:")
-            printf("a: %f, b: %f, c: %f \n", a, b, c);
+            // printf("LINE SEGMENT x1: %f y1: %f ...... x2L %f y2: %f\n", points[i].x, points[i].y, points[j].x, points[j].y);
+            // printf("a: %f, b: %f, c: %f \n", a, b, c);
 
-            for (int k = 0; k < numPoints; k++) {
-                if (k != i && k != j) {
-                    d = (a * points[k].x) + (b * points[k].y);
-                    // printf("c: %f\n", cross_product(points[i], points[j], points[k]));
-                    printf("d: %f, c: %f \n", d, c);
+            // for (int k = 0; k < numPoints; k++) {
+            //     if (k != i && k != j) {
+            //         d = (a * points[k].x) + (b * points[k].y);
+            //         // printf("c: %f\n", cross_product(points[i], points[j], points[k]));
+            //         // printf("x: %f y: %f d: %f c: %f\n", points[k].x, points[k].y, d, c);
 
-                    if (d > c) {
-                        leftSide++;
-                    } else {
-                        rightSide++;
-                    }
-                }
-            }
-            printf("rs: %d, ls: %d \n", rightSide, leftSide);
-            if (rightSide == 0 || leftSide == 0) {
-                hull[hullIndex] = points[i];
-                hull[hullIndex + 1] = points[j];
-                hullIndex++;
-            }
+            //         if (d > c) {
+            //             leftSide++;
+            //         } else {
+            //             rightSide++;
+            //         }
+            //     }
+            // }
+            // printf("rs: %d, ls: %d \n", rightSide, leftSide);
+            // if (rightSide == 0 || leftSide == 0) {
+            //     // for (int k = 0; k <)
+
+            //     hull[*numHullPoints] = points[i];
+            //     hull[*numHullPoints + 1] = points[j];
+            //     *numHullPoints += 2;
+            // }
         }
     }
-
-    return hull;
-}
-
-int cross_product(struct Point a, struct Point b, struct Point c) {
-    int y1 = b.y - a.y;
-    int y2 = c.y - a.y;
-    int x1 = b.x - a.x;
-    int x2 = c.x - a.x;
-    return y2 * x1 - y1 * x2;
 }
