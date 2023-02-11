cd9d88036ff44ac6d56ee6323e6d3aa2252c378c
Caleb Janhunen <cjanhune@uoguelph.ca>
2023-02-10 23:31:01 -0500

    commit

--- P21.c
+++ P21.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define FALSE 0
+#define TRUE 1
+
 struct Point {
     float x;
     float y;
@@ -11,8 +14,8 @@ void convexHullBF(Point *points, int numPoints, Point *hull, int *numHullPoints)
 
 int main(int argc, char **argv) {
     FILE *fp;
-    char *filename = "testQ2.txt";
-    // char *filename = "data_A2_Q2.txt";
+    // char *filename = "testQ2.txt";
+    char *filename = "data_A2_Q2.txt";
     int fsize;
     Point points[30000];
     char num[20];
@@ -29,8 +32,8 @@ int main(int argc, char **argv) {
         i++;
     }
 
-    Point hull[20];
-    int numPoints = 5, numHullPoints = 0;
+    Point hull[30000];
+    int numPoints = 30000, numHullPoints = 0;
     start = clock();
     convexHullBF(points, numPoints, hull, &numHullPoints);
     end = clock();
@@ -38,6 +41,7 @@ int main(int argc, char **argv) {
     for (int i = 0; i < numHullPoints; i++) {
         printf("x: %f, y: %f \n", hull[i].x, hull[i].y);
     }
+    printf("Number of points: %d", numHullPoints);
     double duration = ((double)end - start) / CLOCKS_PER_SEC;
     printf("Execution time = %f \n", duration);
 
@@ -48,141 +52,102 @@ int main(int argc, char **argv) {
 }
 
 void convexHullBF(Point *points, int numPoints, Point *hull, int *numHullPoints) {
-    float crossProduct;
+    float a, b, c, d, compareVal;
+    int onLeft, onRight, i_isUnique, j_isUnique;
+    int isExtremePoint;
 
     for (int i = 0; i < numPoints; i++) {
+        printf("%d\n", i);
         for (int j = i + 1; j < numPoints; j++) {
+            a = points[j].y - points[i].y;
+            b = points[i].x - points[j].x;
+            c = (points[i].x * points[j].y) - (points[i].y * points[j].x);
+            onRight = 0;
+            onLeft = 0;
+            if (i > 0) {
+                compareVal = (a * points[0].x) + (b * points[0].y) - c;
+            } else {
+                compareVal = (a * points[3].x) + (b * points[3].y) - c;
+            }
+            isExtremePoint = TRUE;
             for (int k = 0; k < numPoints; k++) {
-                printf("%d %d %d\n", i, j, k);
-                crossProduct = (points[j].x - points[i].x) * (points[k].y - points[i].y) - (points[k].x - points[i].x) * (points[j].y - points[i].y);
-                printf("%f \n", crossProduct);
-                printf("(%f, %f), (%f, %f), (%f, %f)\n\n", points[i].x, points[i].y, points[j].x, points[j].y, points[k].x, points[k].y);
-                if (crossProduct > 0) {
-                    // printf("hull: %d \n", *numHullPoints);
-                    hull[*numHullPoints] = points[j];
-                    *numHullPoints += 1;
-                    // break;
+                if (k != i && k != j) {
+                    // printf("%d %d %d\n", i, j, k);
+                    // printf("(%f, %f), (%f, %f), (%f, %f)\n", points[i].x, points[i].y, points[j].x, points[j].y, points[k].x, points[k].y)S
+                    d = (a * points[k].x) + (b * points[k].y) - c;
+                    if (d > 0) {
+                        onLeft++;
+                    } else if (d < 0) {
+                        onRight++;
+                    }
+
+                    if (onLeft > 0 && onRight > 0) {
+                        isExtremePoint = FALSE;
+                        break;
+                    }
                 }
             }
+            // printf("onLeft: %d onRight: %d\n\n", onLeft, onRight);
+            if (isExtremePoint == TRUE) {
+                // printf("(%f, %f), (%f, %f), (%f, %f)\n", points[i].x, points[i].y, points[j].x, points[j].y);
+                i_isUnique = TRUE;
+                j_isUnique = TRUE;
+                for (int k = 0; k < *numHullPoints; k++) {
+                    // printf("TEST\n");
+                    if (hull[k].x == points[i].x && hull[k].y == points[i].y) {
+                        i_isUnique = FALSE;
+                    }
+                    if (hull[k].x == points[j].x && hull[k].y == points[j].y) {
+                        j_isUnique = FALSE;
+                    }
+                }
+                // printf("junique: %d, iunique: %d \n", j_isUnique, i_isUnique);
+                // printf("hull: %d\n", *numHullPoints);
+                if (j_isUnique == TRUE && i_isUnique == TRUE) {
+                    hull[*numHullPoints] = points[i];
+                    hull[*numHullPoints + 1] = points[j];
+                    *numHullPoints += 2;
+                } else if (j_isUnique == FALSE && i_isUnique == TRUE) {
+                    hull[*numHullPoints] = points[i];
+                    *numHullPoints += 1;
+                } else if (j_isUnique == TRUE && i_isUnique == FALSE) {
+                    hull[*numHullPoints] = points[j];
+                    *numHullPoints += 1;
+                }
+                // printf("hull after adding: %d\n", *numHullPoints);
+            }
+            // if (isExtreme == TRUE) {
+
+            // }
         }
     }
 }
 
 // void convexHullBF(Point *points, int numPoints, Point *hull, int *numHullPoints) {
-//     float a, b, c, d;
-//     int leftSide, rightSide;
+//     float crossProduct;
 
 //     for (int i = 0; i < numPoints; i++) {
-
-//         for (int j = 0; j < numPoints; j++) {
-//             if (j != i) {
-//                 a = points[j].y - points[i].y;
-//                 b = points[i].x - points[j].x;
-//                 c = (points[i].x * points[j].y) - (points[i].y * points[j].x);
-//                 leftSide = 0;
-//                 rightSide = 0;
-//                 for (int k = 0; k < numPoints; k++) {
-//                     if (k != j && k != i) {
-//                         d = (a * points[k].x) + (b * points[k].y) - c;
-//                         printf("LINE SEGMENT x1: %f y1: %f ...... x2L %f y2: %f............ POINT x: %f, y: %f \n", points[i].x, points[i].y, points[j].x, points[j].y, points[k].x, points[k].y);
-//                         if (d > 0) {
-//                             leftSide++;
-//                         } else {
-//                             rightSide++;
-//                         }
-//                     }
-//                 }
-
-//                 // printf("x: %f y: %f d: %f c: %f\n", points[k].x, points[k].y, d);
-
-//                 // printf("i: %d, j: %d, k: %d************************** \n", i, j, k);
-//                 printf("rs: %d, ls: %d \n", rightSide, leftSide);
-//                 if (rightSide == 0 || leftSide == 0) {
-//                     // for (int k = 0; k <)
-
-//                     hull[*numHullPoints] = points[i];
-//                     hull[*numHullPoints + 1] = points[j];
-//                     *numHullPoints += 2;
+//         for (int j = i + 1; j < numPoints; j++) {
+//             for (int k = j + 1; k < numPoints; k++) {
+//                 printf("%d %d %d\n", i, j, k);
+//                 crossProduct = (points[j].x - points[i].x) * (points[k].y - points[i].y) - (points[k].x - points[i].x) * (points[j].y - points[i].y);
+//                 printf("%f \n", crossProduct);
+//                 printf("(%f, %f), (%f, %f), (%f, %f)\n\n", points[i].x, points[i].y, points[j].x, points[j].y, points[k].x, points[k].y);
+//                 if (crossProduct > 0) {
+//                     // printf("hull: %d \n", *numHullPoints);
+//                     hull[*numHullPoints] = points[j];
+//                     *numHullPoints += 1;
+//                     // break;
 //                 }
 //             }
-
-// printf("LINE SEGMENT x1: %f y1: %f ...... x2L %f y2: %f\n", points[i].x, points[i].y, points[j].x, points[j].y);
-// printf("a: %f, b: %f, c: %f \n", a, b, c);
-
-// for (int k = 0; k < numPoints; k++) {
-//     if (k != i && k != j) {
-//         d = (a * points[k].x) + (b * points[k].y);
-//         // printf("c: %f\n", cross_product(points[i], points[j], points[k]));
-//         // printf("x: %f y: %f d: %f c: %f\n", points[k].x, points[k].y, d, c);
-
-//         if (d > c) {
-//             leftSide++;
-//         } else {
-//             rightSide++;
 //         }
 //     }
 // }
-// printf("rs: %d, ls: %d \n", rightSide, leftSide);
-// if (rightSide == 0 || leftSide == 0) {
-//     // for (int k = 0; k <)
-
-//     hull[*numHullPoints] = points[i];
-//     hull[*numHullPoints + 1] = points[j];
-//     *numHullPoints += 2;
-// }
-// }
-// }
-// }
 
 /*
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct Point {
-  int x;
-  int y;
-} Point;
-
-int isLeftTurn(Point a, Point b, Point c) {
-  return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x) > 0;
-}
-
-Point *convexHull(Point *points, int numPoints, int *numHullPoints) {
-  Point *hull = malloc(numPoints * sizeof(Point));
-  int hullSize = 0;
-  for (int i = 0; i < numPoints; i++) {
-    for (int j = i + 1; j < numPoints; j++) {
-      for (int k = j + 1; k < numPoints; k++) {
-        if (isLeftTurn(points[i], points[j], points[k])) {
-          hull[0] = points[i];
-          hull[1] = points[j];
-          hull[2] = points[k];
-          hullSize = 3;
-          break;
-        }
-      }
-    }
-    for (int j = 0; j < numPoints; j++) {
-      if (hullSize == 0 || j == hull[0].x) continue;
-      while (hullSize >= 2 && !isLeftTurn(points[i], hull[hullSize - 1], points[j])) {
-        hullSize--;
-      }
-      hull[hullSize++] = points[j];
-    }
-  }
-  *numHullPoints = hullSize;
-  return hull;
-}
-
-int main() {
-  Point points[] = {{0, 0}, {0, 1}, {1, 1}, {1, 0}};
-  int numPoints = sizeof(points) / sizeof(Point);
-  int numHullPoints;
-  Point *hull = convexHull(points, numPoints, &numHullPoints);
-  for (int i = 0; i < numHullPoints; i++) {
-    printf("(%d, %d)\n", hull[i].x, hull[i].y);
-  }
-  free(hull);
-  return 0;
-}
+23 -35.2
+45 5
+2 4
+2.4 64
+-4.6 22
 */
