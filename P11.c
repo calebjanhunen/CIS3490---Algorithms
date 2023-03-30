1a1037dab856b614583b79c08c2d5a83270d040f
Caleb Janhunen <cjanhune@uoguelph.ca>
2023-03-30 18:18:13 -0400

    commit

--- P11.c
+++ P11.c
@@ -11,14 +11,15 @@ int main(int argc, char **argv) {
     char *filename = "data_A5_Q1_1.txt";
     Set *set;
 
-    FILE *fp = open_file(filename);
-    set = init_set(fp);
+    FILE *fp = open_file(filename); // open file provided
+    set = init_set(fp);             // Read values from file into array of integers
 
-    printf("%d \n", set->length);
-    for (int i = 0; i < set->length; i++) {
-        printf("%d ", set->nums[i]);
-    }
-    printf("\n");
+    // PRINT SET STRUCT
+    // printf("%d \n", set->length);
+    // for (int i = 0; i < set->length; i++) {
+    //     printf("%d ", set->nums[i]);
+    // }
+    // printf("\n");
 
     free_set(set);
     return 0;
